dc730f89b362f6cbddcfd41e51dd1fbd551d4410
jjub0217 <noonssub1004@gmail.com>
2020-08-06 14:56:54 +0900

    modi: main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-// let liList = document.querySelectorAll('.nav__menuItem a');
-// let liList1 = document.querySelectorAll('.nav__subMenu');
-
-
-// let newArr = Array.from(liList);
-
-
-// newArr[0].addEventListener('mouseover', function() {
-//   toggleElements();
-// })
-
-
-
-
-
 (function( document){
   'use strict'
-
   const $nav = document.getElementById('nav__menu');
   console.log($nav);
   //isAct 추가
@@ -25,23 +9,20 @@
     }
     console.log(e.target);
     const subM =e.target.nextElementSibling; /*<- ul : nav__subMenu  */
-    e.target.nextElementSibling.classList.add('isAct');
+    e.target.nextElementSibling.classList.toggle('isAct');
     // test.classList.add('isAct');
-    
     // isAct 제거a
     $nav.onmouseout =(e) => {
       if(!e.target.matches('.nav__menu > li > a ')) {
         return;
       }
-      e.target.nextElementSibling.classList.remove('isAct');
+      e.target.nextElementSibling.classList.toggle('isAct');
     }
     subM.onmouseover = e =>{
-      subM.classList.add('isAct')
+      subM.classList.toggle('isAct')
       subM.onmouseout = e => {
-        subM.classList.remove('isAct')
+        subM.classList.toggle('isAct')
       }
     }
   }
-
-
 })( document)
